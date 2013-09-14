197ed88b22cce79b2d662a1b28bd5d7a931fb2c0
Admin <admin@Admins-MacBook-Pro.local>
2013-09-13 20:29:03 -0700

    Add link to intent

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130914030405) do
+ActiveRecord::Schema.define(version: 20130914031601) do
 
   create_table "tweets", force: true do |t|
     t.string   "body"
